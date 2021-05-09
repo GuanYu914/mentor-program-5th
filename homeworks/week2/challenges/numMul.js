ce047985767d62a0e542d2f6a67fb7a5df422741
Emory <aa774281@gmail.com>
2021-05-09 12:35:42 +0800

    rewrite codes with eslint

--- homeworks/week2/challenges/numMul.js
+++ homeworks/week2/challenges/numMul.js
@@ -1,66 +1,64 @@
 // 不能使用 BigInt 這個資料型態
 function multiply(a, b) {
+  const calLength = a.length + b.length
+  const calArrOfA = []
+  const calArrOfB = []
+  const calArrSum = []
+  let numCarry = 0
+  let res = ''
 
-    calLength = a.length + b.length
-    calArrOfA = []
-    calArrOfB = []
-    calArrSum = []
-    var numCarry = 0
-    var res = ''
+  // 初始化陣列
+  for (let i = 0; i < calLength; i++) {
+    calArrOfA[i] = 0
+    calArrOfB[i] = 0
+    calArrSum[i] = 0
+  }
+  // 將數值移動到陣列最右邊
+  for (let i = 0; i < a.length; i++) {
+    calArrOfA[i + calLength - a.length] = Number(a[i])
+  }
 
-    // 初始化陣列
-    for (var i = 0; i < calLength; i++) {
-        calArrOfA[i] = 0
-        calArrOfB[i] = 0
-        calArrSum[i] = 0
+  for (let i = 0; i < b.length; i++) {
+    calArrOfB[i + calLength - b.length] = Number(b[i])
+  }
+
+  //  console.log(calArrOfA)
+  //  console.log(calArrOfB)
+
+  // 直式乘法計算
+  for (let i = 0; i < b.length; i++) {
+    for (let j = 0; j < a.length; j++) {
+      // console.log('B[',calLength-1-i,']: ',calArrOfB[calLength - 1 - i])
+      // console.log('A[',calLength-1-j,']: ',calArrOfA[calLength - 1 - j])
+      calArrSum[(calLength - 1) - i - j] += calArrOfB[(calLength - 1) - i] * calArrOfA[calLength - 1 - j]
     }
+  }
 
-    // 將數值移動到陣列最右邊
-    for (var i = 0; i < a.length; i++) {
-        calArrOfA[i + calLength - a.length] = Number(a[i])   
+  // 處理個別進位
+  for (let i = calLength - 1; i >= 0; i--) {
+    numCarry = Math.floor(calArrSum[i] / 10)
+    if (numCarry) {
+      calArrSum[i] %= 10
+      calArrSum[i - 1] += numCarry
     }
+  }
 
-    for (var i = 0; i < b.length; i++) {
-        calArrOfB[i + calLength - b.length] = Number(b[i])
+  // 轉成字串陣列格式
+  let disFlag = false
+  for (let i = 0; i < calArrSum.length; i++) {
+    if (calArrSum[i]) { // 隱蔽 0 的輸出，直到碰到不為 0 的整數
+      disFlag = true
     }
-
-    //  console.log(calArrOfA)
-    //  console.log(calArrOfB)
-
-    // 直式乘法計算
-    for (var i = 0; i < b.length; i++) {
-        for (var j = 0; j < a.length; j++) {
-            // console.log('B[',calLength-1-i,']: ',calArrOfB[calLength - 1 - i])
-            // console.log('A[',calLength-1-j,']: ',calArrOfA[calLength - 1 - j])
-            calArrSum[(calLength - 1) - i - j] += calArrOfB[(calLength - 1) - i] * calArrOfA[calLength - 1 - j]
-        }
+    if (disFlag) {
+      res += calArrSum[i].toString()
     }
+  }
 
-    // 處理個別進位
-    for (var i = calLength-1; i >= 0; i--) {
-        numCarry = Math.floor(calArrSum[i] / 10)
-        if (numCarry) {
-            calArrSum[i] %= 10
-            calArrSum[i-1] += numCarry
-        }
-    }
-
-    // 轉成字串陣列格式
-    var disFlag = false
-    for (var i = 0; i < calArrSum.length; i++) {
-        if (calArrSum[i]) { // 隱蔽 0 的輸出，直到碰到不為 0 的整數
-            disFlag = true
-        }
-        if (disFlag) {
-            res += calArrSum[i].toString()
-        }
-    }
-
-    return res
+  return res
 }
 
 // 結果輸出
-var testNum = multiply('999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999', '999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999')
+const testNum = multiply('999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999', '999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999')
 console.log('結果輸出')
 console.log(testNum)
 
--- homeworks/week2/challenges/search.js
+++ homeworks/week2/challenges/search.js
@@ -1,30 +1,26 @@
 // fast ver.
 // 基於以排序完成的陣列，可以使用二元搜尋加速效率
-function search (arr, n) {
-    
-    var low, high
-    low = 0
-    high = arr.length - 1
+function search(arr, n) {
+  const low = 0
+  const high = arr.length - 1
 
-    return BinSearch(arr, n, low, high)
+  return BinSearch(arr, n, low, high)
 }
 
-function BinSearch (arr, n, low, high) {
-    var mid 
-
-    if (high >= low) {
-        mid = Math.ceil((high + low) / 2)
-
-        if(n > Number(arr[mid])) {
-            return BinSearch(arr, n, mid+1, high)
-        } else if (n < Number(arr[mid])) {
-            return BinSearch(arr, n, low, mid-1)
-        } else if (n === Number(arr[mid])){
-            return mid
-        } 
-    } else {
-        return -1
+function BinSearch(arr, n, low, high) {
+  let mid
+  if (high >= low) {
+    mid = Math.ceil((high + low) / 2)
+    if (n > Number(arr[mid])) {
+      return BinSearch(arr, n, mid + 1, high)
+    } else if (n < Number(arr[mid])) {
+      return BinSearch(arr, n, low, mid - 1)
+    } else if (n === Number(arr[mid])) {
+      return mid
     }
+  } else {
+    return -1
+  }
 }
 
 /* slower ver.
