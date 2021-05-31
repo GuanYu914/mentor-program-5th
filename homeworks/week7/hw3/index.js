5a7f66da98c30446fa7aea24d479a14ece7735b2
Emory <aa774281@gmail.com>
2021-05-31 19:00:57 +0800

    rewrite codes with better way

--- homeworks/week7/hw3/index.js
+++ homeworks/week7/hw3/index.js
@@ -3,54 +3,24 @@ document.querySelector('input[name=item_default_text]').addEventListener('keydow
   if (e.key === 'Enter') {
     console.log('Enter detected!')
     if (document.querySelector('input[name=item_default_text]').value === '') return
+
     // 產生 Todo list html 所需標籤
-    // <div> class = todoList__content__item_new
-    const todoListItemNew = document.createElement('div')
-    todoListItemNew.classList.add('todoList__content__item_new')
+    const taskName = document.querySelector('input[name=item_default_text]').value
+    const itemNew = document.createElement('div')
+    itemNew.classList.add('todoList__content__item_new')
+    itemNew.innerHTML = `<div class="todoList__content__item_new__above_underline">
+    <label class="todoList__content__item_new__checkbox_container">
+      <input type="checkbox" name="item_new_checkbox">
+      <span class="todoList__content__item_new__checkbox_container__checkmark"></span>
+      <p class="todoList__content__item_new__checkbox_container__task_name">${taskName}</p>
+    </label>
+    <button class="todoList__content__item_new__delete"></button>
+  </div>
+  <div class="todoList__content__item_new__underline"></div>`
 
-    // <div> class = todoList__content__item_new__above_underline
-    const todoListItemNewAboveUnderline = document.createElement('div')
-    todoListItemNewAboveUnderline.classList.add('todoList__content__item_new__above_underline')
-
-    // <label> class = todoList__content__item_new__checkbox_container
-    const todoListItemNewCheckboxContainer = document.createElement('label')
-    todoListItemNewCheckboxContainer.classList.add('todoList__content__item_new__checkbox_container')
-
-    // <input type="checkbox" name="item_new_checkbox"/>
-    const todoListItemNewCheckboxContainerCheckbox = document.createElement('input')
-    todoListItemNewCheckboxContainerCheckbox.type = 'checkbox'
-    todoListItemNewCheckboxContainerCheckbox.name = 'item_new_checkbox'
-
-    // <span> class = todoList__content__item_new__checkbox_container__checkmark
-    const todoListItemNewCheckboxContainerCheckmark = document.createElement('span')
-    todoListItemNewCheckboxContainerCheckmark.classList.add('todoList__content__item_new__checkbox_container__checkmark')
-
-    // <p> class = todoList__content__item_new__checkbox_container__task_name
-    const todoListItemNewCheckboxContainerTaskname = document.createElement('p')
-    todoListItemNewCheckboxContainerTaskname.classList.add('todoList__content__item_new__checkbox_container__task_name')
-    todoListItemNewCheckboxContainerTaskname.innerText = document.querySelector('input[name=item_default_text]').value
-
-    // <button> class = todoList__content__item_new__delete
-    const todoListItemNewDelete = document.createElement('button')
-    todoListItemNewDelete.classList.add('todoList__content__item_new__delete')
-
-    // <div> class = todoList__content__item_new__underline
-    const todoListItemNewUnderline = document.createElement('div')
-    todoListItemNewUnderline.classList.add('todoList__content__item_new__underline')
-
-    // 產生 Todo List html 架構
-    todoListItemNewCheckboxContainer.appendChild(todoListItemNewCheckboxContainerCheckbox)
-    todoListItemNewCheckboxContainer.appendChild(todoListItemNewCheckboxContainerCheckmark)
-    todoListItemNewCheckboxContainer.appendChild(todoListItemNewCheckboxContainerTaskname)
-
-    todoListItemNewAboveUnderline.appendChild(todoListItemNewCheckboxContainer)
-    todoListItemNewAboveUnderline.appendChild(todoListItemNewDelete)
-
-    todoListItemNew.appendChild(todoListItemNewAboveUnderline)
-    todoListItemNew.appendChild(todoListItemNewUnderline)
-
+    // 新增 Todo list 清單
     const rootElement = document.querySelector('.todoList__content')
-    rootElement.appendChild(todoListItemNew)
+    rootElement.appendChild(itemNew)
 
     // 清空 default input 輸入框的數值
     document.querySelector('input[name=item_default_text]').value = ''
