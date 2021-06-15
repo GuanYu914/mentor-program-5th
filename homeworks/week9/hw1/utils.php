1a7c552e53e9acfe02fb119cf818b5eec07f45ce
Emory <aa774281@gmail.com>
2021-06-15 13:53:21 +0800

    fix global var issue

--- homeworks/week9/hw1/utils.php
+++ homeworks/week9/hw1/utils.php
@@ -1,22 +1,21 @@
 <!-- 放一些常用的 codes -->
 <?php
-
+require_once("macro.php");
 // 解析 SESSION['Msg'] 的內容
 function getErrMsg()
 {
-  require_once("macro.php");
-
-  if ($_SESSION['Msg'] === $SUCESS_ENROLLMENT) {
+  // 使用 $GLOBALS 拿到 macro.php 的變數
+  if ($_SESSION['Msg'] === $GLOBALS['SUCESS_ENROLLMENT']) {
     $Msg = "註冊成功";
-  } else if ($_SESSION['Msg'] === $EMPTY_INPUT_DATA) {
+  } else if ($_SESSION['Msg'] === $GLOBALS['EMPTY_INPUT_DATA']) {
     $Msg = "欄位不能為空";
-  } else if ($_SESSION['Msg'] === $USED_USERNAME) {
+  } else if ($_SESSION['Msg'] === $GLOBALS['USED_USERNAME']) {
     $Msg = "此帳號名稱已被使用";
-  } else if ($_SESSION['Msg'] === $ACCOUNT_NOT_FOUNDED) {
+  } else if ($_SESSION['Msg'] === $GLOBALS['ACCOUNT_NOT_FOUNDED']) {
     $Msg = "找不到此帳密組合，請再試一次";
-  } else if ($_SESSION['Msg'] === $EMPTY_COMMENT) {
+  } else if ($_SESSION['Msg'] === $GLOBALS['EMPTY_COMMENT']) {
     $Msg = "留言還沒填呢";
-  } else if ($_SESSION['Msg'] === $ERROR_SERVER_SIDE) {
+  } else if ($_SESSION['Msg'] === $GLOBALS['ERROR_SERVER_SIDE']) {
     $Msg = "伺服器端發生錯誤，請稍後再試";
   }
 
